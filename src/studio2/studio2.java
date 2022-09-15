299b57a3bc5ef96dc41091f982bab6efba913108
qingtan <qingtan@tracys-mbp.dhcp.wustl.edu>
2022-09-15 12:48:30 -0500

    Studio 2

---
+++ src/studio2/studio2.java
@@ -0,0 +1,62 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class studio2 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int startAmount, winLimit;
+		double winChance;
+		int simulation = 0;
+		int round = 1;
+		int ruin = 0;
+		boolean status;
+		System.out.print("Input starting amount of money: ");
+		startAmount = in.nextInt();
+		
+		winLimit = 50;
+		
+		while (simulation <500) {
+			System.out.println("NEW SIMULATION STARTING");
+			round =1;
+			while (startAmount < winLimit && startAmount != 0) 
+			{
+				System.out.println(round);
+				winChance = Math.random();
+				if (winChance >= 0.5) {
+					startAmount++;
+					System.out.println("won money");
+					round++;
+				}
+				else {
+					startAmount--;
+					System.out.println("lost money");
+					round++;
+				}
+			}
+			
+			if (startAmount == winLimit) {
+				simulation++;
+				System.out.println("won");
+				//break;
+			}
+			else if (startAmount == 0){
+				simulation++;
+				System.out.println("lose");
+				//break;
+			}
+//			if (startAmount == winLimit) {
+//				simulation++;
+//				System.out.println("Simulation " + simulation + ": " + round + " WIN " + startAmount);
+//			}
+//			else if (startAmount == 0) {	
+//				simulation ++;
+//				System.out.println("Simulation " + simulation + ": " + round + " LOSE " + startAmount);
+//				ruin++;
+//			}
+
+		}
+	}
+
+}
